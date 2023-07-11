f984c6d67dc912146cc417c9fcbe5ddd464398a7
roona.kumari <roona.kumari@niit.com>
2023-07-11 13:48:44 +0530

    file structure added

---
+++ src/main/java/com/roona/NoDuplicateInHashMap/NoDuplicateInHashMapApplication.java
@@ -0,0 +1,13 @@
+package com.roona.NoDuplicateInHashMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoDuplicateInHashMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoDuplicateInHashMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roona/NoDuplicateInHashMap/NoDuplicateInHashMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roona.NoDuplicateInHashMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoDuplicateInHashMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
